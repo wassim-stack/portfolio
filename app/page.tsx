bf0d9fb3a1fecd7ba4bc798c3ddf6f5658bb8b75
werhani wassim <werhaniwassim@kelibia.r-iset.tn>
2023-10-16 10:26:07 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import AboutSection from "@/components/AboutSection";
+import HeroSection from "@/components/HeroSection";
+import ProjectSection from "@/components/ProjectSection";
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
+      <AboutSection />
+      <ProjectSection />
+    </main>
+  );
+}
